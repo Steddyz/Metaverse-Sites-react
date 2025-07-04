44894f7695915eaad302a0b0e802782b3e9b8ec1
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-07-04 15:56:39 +0300

    add GetStarted section

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,18 @@
-const StartSteps = () => (
-  <div>
-    start steps
+import styles from "../styles";
+
+const StartSteps = ({ text, number }) => (
+  <div className={`${styles.flexCenter} flex-row`}>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
+    >
+      <p className="text-white font-bold text-[20px]">0{number}</p>
+    </div>
+    <p
+      className="flex-1 font-normal ml-[30px] text-[18px]
+       leading-[32px] text-[#B0B0B0]"
+    >
+      {text}
+    </p>
   </div>
 );
 
