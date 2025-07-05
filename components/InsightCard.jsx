beea7be435f8b6f4dc622b575592b9afeb9b8574
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-07-05 16:19:50 +0300

    add Insight component

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,36 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
 
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+import { fadeIn } from "../utils/motion";
+
+const InsightCard = ({ title, subtitle, imgUrl, index }) => (
+  <motion.div
+    variants={fadeIn("up", "spring", index * 0.5, 1)}
+    className="flex md:flex-row flex-col gap-4"
+  >
+    <img
+      src={imgUrl}
+      alt={title}
+      className="rounded-[32px] md:w-[270px] w-full h-[250px] object-cover"
+    />
+    <div className="flex justify-between items-center w-full ">
+      <div className="flex flex-col flex-1 md:ml-[62px] max-w-[650px]">
+        <h4 className="font-normal lg:text-[42px] text-[26px] text-white">
+          {title}
+        </h4>
+        <p className="font-normal lg:text-[20px] text-[14px] mt-[16px] text-secondary-white">
+          {subtitle}
+        </p>
+      </div>
+      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white cursor-pointer">
+        <img
+          src="arrow.svg"
+          alt="arrow"
+          className="w-[40%] h-[40%] object-contain"
+        />
+      </div>
+    </div>
+  </motion.div>
 );
 
 export default InsightCard;
--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,37 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainers } from "../utils/motion";
+
+import { TypingText, InsightCard, TitleText } from "../components";
+import { insights } from "../constants";
 
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainers}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText
+        title={<>Insight about metaverse</>}
+        textStyles="text-center"
+      />
+      <div className="flex flex-col mt-[50px] gap-[30px]">
+        {insights.map((insight, index) => (
+          <InsightCard
+            key={`insight-${index}`}
+            {...insight}
+            index={index + 1}
+          />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
